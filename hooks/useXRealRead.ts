320582c591c1e9133d092e085c31d29871461889
ademmeral <kmltuzer@gmail.com>
2023-11-18 00:24:22 +0300

    some changes

--- hooks/useXRealRead.ts
+++ hooks/useXRealRead.ts
@@ -1,8 +1,8 @@
 'use client';
 // https://github.com/ademmeral/XReact/hooks/useXRealRead.ts
 
-import { useEffect, useState, useRef, MutableRefObject } from "react";
-
+import { useEffect, useState, MutableRefObject } from "react";
+// import xInterval from './xInterval';
 
 type UseXRealRead = (ref: MutableRefObject<any>, readSpeed: number) => {
   startTime: number,
@@ -11,11 +11,38 @@ type UseXRealRead = (ref: MutableRefObject<any>, readSpeed: number) => {
   restart : () => void
 };
 
+function xInterval(callback:any, interval:number) {
+  let startTime = 0;
+  let reqId:any;
+  function loop(timestamp:any) {
+    if (!startTime) {
+      startTime = timestamp;
+    }
+
+    const elapsed = timestamp - startTime;
+
+    if (elapsed >= interval) {
+      callback();
+      startTime = timestamp;
+    }
+
+    reqId = window.requestAnimationFrame(loop);
+  }
+
+  reqId = window.requestAnimationFrame(loop);
+
+  return {
+    clear : function(){cancelAnimationFrame(reqId)}
+  }
+}
+
 export const useXRealRead: UseXRealRead = (ref, readSpeed) => {
   const [isRead, setIsRead] = useState<boolean | null>(null);
   const [elapsedTime, setElapsedTime] = useState<number>(0);
   const [observer, setObserver] = useState(new IntersectionObserver(callback))
-  let { current: startTime } = useRef(Date.now());
+  const [interval, setIntv] = useState<ReturnType<typeof xInterval>>();
+  const [startTime, setStartTime] = useState<number>(0);
+
 
   function callback (entries:IntersectionObserverEntry[]) {
     const targ = entries[0];
@@ -35,15 +62,35 @@ export const useXRealRead: UseXRealRead = (ref, readSpeed) => {
   };
 
   function startObserver(){
+    setStartTime(Date.now())
     const lastSpan = document.createElement('span');
     lastSpan.setAttribute('class', 'read-section last');
     ref.current.appendChild(lastSpan, ref.current.lastChild);
 
     observer.observe(lastSpan);
   }
-
+  
   useEffect(() => {
-    startObserver();
+    if (elapsedTime >= readSpeed) {
+      setIsRead(true);
+      interval?.clear();
+    }
+  }, [elapsedTime])
+  
+  useEffect(() => {
+    const refObserver = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting) {
+        if (ref.current.clientHeight <= window.innerHeight) {
+          setStartTime(Date.now())
+          const intv = xInterval(() => {
+            setElapsedTime(p => p += 2000);
+          }, 2000);
+          setIntv(intv)
+        }
+      } else startObserver();
+    });
+    refObserver.observe(ref.current);
+
     return () => {
       observer.disconnect();
     };
